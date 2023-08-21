6070d38c76ecf7590338eb3c805f694f7baa5d7e
ahdy101 <100008355+ahdy101@users.noreply.github.com>
2023-08-21 14:54:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,74 @@
-  const form = document.getElementById('registration-form');
-  const passwordInput = document.getElementById('password');
-  const confirmPasswordInput = document.getElementById('confirmPassword');
-  const passwordError = document.getElementById('password-error');
-  const confirmPasswordError = document.getElementById('confirm-password-error');
+const form = document.getElementById('registration-form');
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const confirmPasswordInput = document.getElementById('confirmPassword');
 
-  const validatePassword = password => /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/.test(password);
+const nameSuccess = document.getElementById('name-success');
+const emailSuccess = document.getElementById('email-success');
+const passwordSuccess = document.getElementById('password-success');
+const confirmPasswordSuccess = document.getElementById('confirm-password-success');
+const successMessage = document.getElementById('success-message');
 
-  form.addEventListener('submit', event => {
-    if (!validatePassword(passwordInput.value)) {
-      event.preventDefault();
-      passwordError.textContent = "Password must contain at least one number, one symbol, and be 8 characters or longer.";
-    } else {
-      passwordError.textContent = "";
-    }
+const validateName = name => /^[A-Za-z]{2,}$/.test(name);
 
-    if (passwordInput.value !== confirmPasswordInput.value) {
-      event.preventDefault();
-      confirmPasswordError.textContent = "Passwords do not match.";
-    } else {
-      confirmPasswordError.textContent = "";
-    }
-  });
+const validateEmail = email => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+
+const validatePassword = password => /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/.test(password);
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  let isValid = true;
+
+  if (nameInput.value.trim() === '') {
+    nameSuccess.textContent = '';
+    nameInput.classList.add('input-error');
+    isValid = false;
+  } else if (!validateName(nameInput.value)) {
+    nameSuccess.textContent = 'Name must contain at least 2 letters.';
+    nameInput.classList.add('input-error');
+    isValid = false;
+  } else {
+    nameSuccess.textContent = 'Valid!';
+    nameInput.classList.remove('input-error');
+  }
+
+  if (emailInput.value.trim() === '') {
+    emailSuccess.textContent = '';
+    emailInput.classList.add('input-error');
+    isValid = false;
+  } else if (!validateEmail(emailInput.value)) {
+    emailSuccess.textContent = 'Email is not valid.';
+    emailInput.classList.add('input-error');
+    isValid = false;
+  } else {
+    emailSuccess.textContent = 'Valid!';
+    emailInput.classList.remove('input-error');
+  }
+
+  if (passwordInput.value === '') {
+    passwordSuccess.textContent = '';
+    passwordInput.classList.add('input-error');
+    isValid = false;
+  } else if (!validatePassword(passwordInput.value)) {
+    passwordSuccess.textContent = 'Password must be at least 8 characters long and contain at least one uppercase letter and one number or symbol.';
+    passwordInput.classList.add('input-error');
+    isValid = false;
+  } else {
+    passwordSuccess.textContent = 'Valid!';
+    passwordInput.classList.remove('input-error');
+  }
+
+  if (confirmPasswordInput.value === '') {
+    confirmPasswordSuccess.textContent = '';
+    confirmPasswordInput.classList.add('input-error');
+    isValid = false;
+  } else if (passwordInput.value !== confirmPasswordInput.value) {
+    confirmPasswordSuccess.textContent = 'Passwords do not match.';
+    confirmPasswordInput.classList.add('input-error');
+    isValid = false;
+  } else {
+    confirmPasswordSuccess.textContent = 'Valid!';
+    confirmPasswordInput.classList.remove('input-error');
+  }
+});
